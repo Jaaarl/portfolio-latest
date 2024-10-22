df2c19622bad398ae06734b68e393e0209d8a354
jaaarl <jarl13.work@gmail.com>
2024-10-23 00:46:09 +0800

    add: hero

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 function App() {
   return (
     <> 
     <Navbar />
+    <Hero />
     </>
   )
 }
