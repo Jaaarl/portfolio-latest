87258aa89397e7fcf61acb2f92af9e077efea02d
Jarl <jarl13.work@gmail.com>
2025-01-16 23:41:41 +0800

    add: github, cv, linkenid

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,6 @@
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa6";
+import { FaFileAlt } from "react-icons/fa";
 const Hero = () => {
   return (
     <div className="hero bg-base-200 min-h-screen">
@@ -10,7 +13,25 @@ const Hero = () => {
         <p className="py-6">
         A versatile full-stack developer with expertise in Ruby on Rails and Next.js, adept at building scalable, high-performance web applications from front-end to back-end.
         </p>
-        <button className="btn btn-primary">Get Started</button>
+        <div className="flex items-center gap-2">
+          <a href="Empleo_CV.pdf" download>
+            <button className="btn btn-primary">
+              <FaFileAlt /> Download CV
+            </button>
+          </a>
+
+          <a href="https://github.com/Jaaarl" target="_blank" rel="noopener noreferrer">
+            <button className="btn btn-success">
+              <FaGithub className="text-white"/> Github
+            </button>
+          </a>
+
+          <a href="https://www.linkedin.com/in/jarlempleo13" target="_blank" rel="noopener noreferrer">
+            <button className="btn btn-info">
+              <FaLinkedin className="text-white"/> LinkedIn
+            </button>
+          </a>
+        </div>
       </div>
     </div>
   </div>
