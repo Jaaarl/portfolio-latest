1afc8201a723124ad104a179340f2c6fc48ad829
Jarl <jarl13.work@gmail.com>
2025-01-16 15:30:03 +0800

    fix: vercel not deploying because of its not esm

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-/** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui"
 export default {
   content: [
     "./index.html",
@@ -8,7 +8,7 @@ export default {
     extend: {},
   },
   plugins: [
-    require('daisyui'),
+    daisyui,
   ],
 }
 
