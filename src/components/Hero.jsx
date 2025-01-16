1c236be70afddc06ab4819e62bfaae089ae81114
Jarl <jarl13.work@gmail.com>
2025-01-16 22:52:11 +0800

    modify: add description to hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,20 +1,20 @@
 const Hero = () => {
   return (
     <div className="hero bg-base-200 min-h-screen">
-      <div className="hero-content flex-col lg:flex-row-reverse">
-        <img
-          src="/Jarl.jpg"
-          className="max-w-sm rounded-lg shadow-2xl" />
-        <div>
-          <h1 className="text-5xl font-bold">Hi, i'm Jarl!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-            quasi. In deleniti eaque aut repudiandae et a id nisi.
-          </p>
-          <button className="btn btn-primary">Get Started</button>
-        </div>
+    <div className="hero-content flex-col lg:flex-row-reverse gap-8">
+      <img
+        src="/Jarl.jpg"
+        className="max-w-sm rounded-lg shadow-2xl" />
+      <div> 
+        <h1 className="text-5xl font-bold">Hi, I'm Jarl!</h1>
+        <p className="py-6">
+        A versatile full-stack developer with expertise in Ruby on Rails and Next.js, adept at building scalable, high-performance web applications from front-end to back-end.
+        </p>
+        <button className="btn btn-primary">Get Started</button>
       </div>
     </div>
+  </div>
+
   );
 };
 
