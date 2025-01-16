7ab31067ce9051fecb1c91755a5a162c23962aa5
Jarl <jarl13.work@gmail.com>
2025-01-16 17:24:15 +0800

    add: skills component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import Skills from './components/Skills';
 function App() {
   return (
     <> 
     <Navbar />
     <Hero />
     <Projects/>
+    <Skills/>
     <Contact/>
     </>
   )
